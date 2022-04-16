364546492b06065af2cb6485c1b245320269e5e8
nobourge <57195536+nobourge@users.noreply.github.com>
2022-04-16 18:46:26 +0200

    facebook_combined.txt V & E

--- src/Degeneracy.java
+++ src/Degeneracy.java
@@ -1,5 +1,13 @@
 import edu.princeton.cs.algs4.Graph;
 
+/*Dans ce qui suit, on consid´erera des graphes non-dirig´es, simples et sans boucle. Un
+sous-graphe d’un tel graphe G est un graphe obtenu en supprimant certains sommets et
+certaines arˆetes de G. Un graphe G est dit k-d´eg´en´er´e si pour tout sous-graphe de G, y
+compris G lui-mˆeme, il existe un sommet de degr´e au plus k. La d´eg´en´erescence d’un
+graphe est le plus petit nombre k tel que le graphe est k-d´eg´en´er´e. Un exemple de graphe
+3-d´eg´en´er´e est donn´e en Figure 1.
+
+ */
 //given a graph, returns its degeneracy
 public class Degeneracy {
 
