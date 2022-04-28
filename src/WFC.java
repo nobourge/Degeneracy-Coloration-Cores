8b6a66f6aa238cec79cede93f48b2fb95f1139a6
rbecker5 <robin-gilles.becker@uld.be>
2022-04-28 21:19:03 +0200

    WFC optimisation

--- src/WFC.java
+++ src/WFC.java
@@ -3,20 +3,21 @@
 import java.util.*;
 import java.lang.*;
 import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
 
 public class WFC {
 
     static int chromaticNBR = -1;
     static int M=0;
-    static LinkedList<Integer> L = new LinkedList<>();
     static Map<Integer, Integer> coloration = new LinkedHashMap<>();
     static LinkedList<Integer>[] entropy = new LinkedList[]{};
     static List<Integer> uncolored = new ArrayList<>();
+    static Graph G;
 
     public static void main(String[] args) {
         if (args.length == 2) {
-            Graph G = GraphGenerator.generateGraph(args[0], args[1]);
-            solve(G);
+            G = GraphGenerator.generateGraph(args[0], args[1]);
+            solve();
         }
         else {
             //String filename = "ressources/graph/SNAP/roadNet-PA.txt/roadNet-PA.txt";
@@ -26,45 +27,42 @@ public class WFC {
             String delimiter = " ";
             //String filename = "src/musae_FR_edges.csv";
             //String delimiter = ",";
-            Graph G = GraphGenerator.generateGraph(filename, delimiter);
+            G = GraphGenerator.generateGraph(filename, delimiter);
             System.out.println("solving...");
             long start2 = System.currentTimeMillis();
-            solve(G);
+            solve();
             long end2 = System.currentTimeMillis();
             System.out.println("Elapsed Time in milli seconds: "+ (end2-start2));
         }
     }
 
     public static void resetAlgo() {
-        M = 0;
-        L.clear();
         coloration = new LinkedHashMap<>();
         uncolored = new ArrayList<>();
     }
 
-    public static void initAlgo(Graph G) {
+    public static void initAlgo() {
         entropy = new LinkedList[G.V()];
-        int origin=0;
-        for (int i=0; i<G.V(); i++) {
-            int degree = G.degree(i);
+        int origin=0; int degree;
+        for (int i=0; i<G.V(); i++) { //O(V)
+            degree = G.degree(i); //O(1)
             if (degree > M) {
                 M = degree;
                 origin = i;
             }
-            uncolored.add(i);
+            uncolored.add(i); //O(1)
+            entropy[i] = new LinkedList<>();
         }
-        for (int i=0; i<M; i++) { L.add(i); }
-        for (int i=0; i<G.V(); i++) { entropy[i] = new LinkedList<>(); }
-
-        colorize(origin, collapse(origin), true);
-        propagate(G, origin);
+        colorize(origin, collapse(origin)); //O(V)
+        propagate(origin); //O(d*V)?
     }
 
-    public static int solve(Graph G) {
+    public static int solve() { //O(d*V*V^2)
         while(chromaticNBR == -1) {
-            initAlgo(G);
-            while (!uncolored.isEmpty()) {
-                int newOrigin = observe();
+            initAlgo();
+            int newOrigin;
+            while (!uncolored.isEmpty()) { //O(V)
+                newOrigin = observe(); //O(V)
                 if (newOrigin == -1) {
                     chromaticNBR = -1;
                     resetAlgo();
@@ -72,10 +70,9 @@ public class WFC {
                     System.out.println("Restart with M = "+M);
                     break;
                 }
-                colorize(newOrigin, collapse(newOrigin), true);
-                propagate(G, newOrigin);
-                if (uncolored.size() % 1000 == 0) {
-                    System.out.println(uncolored.size()+" left. color used : "+chromaticNBR); }
+                colorize(newOrigin, collapse(newOrigin));
+                propagate(newOrigin); //O(d*V) ?
+                if (uncolored.size() % 1000 == 0) { System.out.println(uncolored.size()+" left. color used : "+chromaticNBR); }
             }
         }
         //System.out.println(coloration);
@@ -84,17 +81,15 @@ public class WFC {
         return chromaticNBR;
     }
 
-    public static void colorize(int v, int color, boolean overwrite) {
-        coloration.put(v, color);
-        uncolored.remove(Integer.valueOf(v));
+    public static void colorize(int v, int color) {
+        coloration.put(v, color); //O(1)
+        uncolored.remove(Integer.valueOf(v)); //O(V)
         chromaticNBR = Math.max(chromaticNBR, color);
-
-        //if (overwrite) { domain[v].removeIf(n -> (n != color)); }
     }
 
     public static int observe() {
         int lowestEntropy = -1; int lowestVertex = -1; int curr_entropy;
-        for (int vertex: uncolored) {
+        for (int vertex: uncolored) { //O(V)
             curr_entropy = entropy(vertex);
             if (curr_entropy > lowestEntropy) {
                 lowestEntropy = curr_entropy;
@@ -105,36 +100,31 @@ public class WFC {
         return lowestVertex;
     }
 
-    public static void propagate(Graph G, int v) {
+    public static void propagate(int v) { //O(d*V)
         Stack<Integer> notPropagated = new Stack<>();
-        notPropagated.add(v);
-        int propagationOrigin;
+        notPropagated.push(v); //O(1)
+
+        int propagationOrigin; int colorOrigin;
         while (!notPropagated.isEmpty()) {
-            propagationOrigin = notPropagated.pop();
-            int colorOrigin = color(propagationOrigin);
-            for (int neigh: G.adj(propagationOrigin)) {
-                //if (entropy(neigh) > 1) { domain[neigh].remove(Integer.valueOf(color(propagationOrigin))); }
-                if (!coloration.containsKey(neigh)) {
-                    if (entropy(neigh) < M-1) {
-                        entropy[neigh].add(colorOrigin); }
+            propagationOrigin = notPropagated.pop(); //O(1)
+            colorOrigin = color(propagationOrigin);
+            for (int neigh: G.adj(propagationOrigin)) { //O(d)
+                if (!coloration.containsKey(neigh)) { //O(V)
+                    if (entropy(neigh) < M-1) { entropy[neigh].add(colorOrigin); }
                     if (entropy(neigh) == M-1) {
-                        colorize(neigh, collapse(neigh), false);
-                        notPropagated.add(neigh);
+                        colorize(neigh, collapse(neigh));
+                        notPropagated.push(neigh);
                     }
                 }
         }   }
     }
 
-    public static int entropy(int v) { return entropy[v].size(); }
+    public static int entropy(int v) { return entropy[v].size(); } //O(1)
 
-    public static int color(int v) { return coloration.get(v); }
+    public static int color(int v) { return coloration.get(v); } //O(1)
 
-    public static int collapse(int v) //{ return domain[v].get(0); }
-    {
-        for (int i=0; i<M; i++) {
-            if (!entropy[v].contains(i)) {
-                return i; }
-        }
+    public static int collapse(int v) {
+        for (int i=0; i<M; i++) { if (!entropy[v].contains(i)) { return i; } } //O(D^2)
         return -1;
     }
 }
